d98054188f3e4f63063e406cce1f2a3fa6693f4a
Justin Clift <justin@postgresql.org>
2017-05-22 16:38:37 +0100

    Save point.  Create commit index and branches

--- newdatamodel.go
+++ newdatamodel.go
@@ -9,7 +9,7 @@ import (
 	"os"
 	"time"
 
-//	"github.com/davecgh/go-spew/spew"
+	//"github.com/davecgh/go-spew/spew"
 )
 
 type branch struct {
@@ -74,20 +74,10 @@ func main() {
 	}
 	entry2.shaSum = sha256.Sum256(tempBuf)
 
-	entry3.aType = DATABASE
-	entry3.name = "testdb3.sqlite"
-	tempBuf, err = ioutil.ReadFile("/Users/jc/tmp/testdb3.sqlite")
-	if err != nil {
-		log.Printf("Something went wrong when reading in the database file: %v\n", err.Error())
-		os.Exit(1)
-	}
-	entry3.shaSum = sha256.Sum256(tempBuf)
-
 	// Populate a dbTree structure with the entries
 	var someTree dbTree
 	someTree.entries = append(someTree.entries, entry1)
 	someTree.entries = append(someTree.entries, entry2)
-	someTree.entries = append(someTree.entries, entry3)
 	someTree.id = createDBTreeID(someTree.entries)
 
 	// Construct an initial commit structure pointing to the entry
@@ -97,9 +87,49 @@ func main() {
 	someCommit.message = "Initial database upload"
 	someCommit.timestamp = time.Now()
 	someCommit.tree = someTree.id
-
 	someCommit.id = createCommitID(someCommit)
 
+	// Create another tree and commit
+	entry3.aType = DATABASE
+	entry3.name = "testdb3.sqlite"
+	tempBuf, err = ioutil.ReadFile("/Users/jc/tmp/testdb3.sqlite")
+	if err != nil {
+		log.Printf("Something went wrong when reading in the database file: %v\n", err.Error())
+		os.Exit(1)
+	}
+	entry3.shaSum = sha256.Sum256(tempBuf)
+
+	var someTree2 dbTree
+	someTree2.entries = append(someTree2.entries, entry3)
+	someTree2.id = createDBTreeID(someTree2.entries)
+
+	var someCommit2 commit
+	someCommit2.parent = someCommit.id
+	someCommit2.authorEmail = "justin@postgresql.org"
+	someCommit2.authorName = "Justin Clift"
+	someCommit2.message = "Added another database"
+	someCommit2.timestamp = time.Now()
+	someCommit2.tree = someTree2.id
+	someCommit2.id = createCommitID(someCommit2)
+
+	// Assemble the commits into an index
+	index = append(index, someCommit)
+	index = append(index, someCommit2)
+
+	// Create a branch
+	var someBranch branch
+	someBranch.name = "master"
+	someBranch.commit = someCommit2.id
+
+	// Create a branch pointing at the initial commit
+	var someBranch2 branch
+	someBranch2.name = "first_commit"
+	someBranch2.commit = someCommit.id
+
+	// Populate the branches variable
+	branches = append(branches, someBranch)
+	branches = append(branches, someBranch2)
+//spew.Dump(index)
 }
 
 func createCommitID(com commit) [32]byte {
@@ -115,19 +145,20 @@ func createCommitID(com commit) [32]byte {
 			com.timestamp.Format(time.UnixDate) + "\n")
 	}
 	b.WriteString("\n" + com.message)
+	b.WriteByte(0) // null byte
 //spew.Dump(b)
 	return sha256.Sum256(b.Bytes())
 }
 
 func createDBTreeID(entries []dbTreeEntry) [32]byte {
-	var buf bytes.Buffer
+	var b bytes.Buffer
 	for _, j := range entries {
-		buf.WriteString(string(j.aType))
-		buf.WriteByte(0) // null byte
-		buf.WriteString(hex.EncodeToString(j.shaSum[:]))
-		buf.WriteByte(0) // null byte
-		buf.WriteString(j.name + "\n")
+		b.WriteString(string(j.aType))
+		b.WriteByte(0) // null byte
+		b.WriteString(hex.EncodeToString(j.shaSum[:]))
+		b.WriteByte(0) // null byte
+		b.WriteString(j.name + "\n")
 	}
-//spew.Dump(buf)
-	return sha256.Sum256(buf.Bytes())
+//spew.Dump(b)
+	return sha256.Sum256(b.Bytes())
 }
