3e8d12cb2e21ccd482c1a736212633e524ded2ab
Justin Clift <justin@postgresql.org>
2017-05-24 15:49:18 +0100

    Stub API server for dio to connect to.
    I'm starting to get a reasonable understanding of what's needed from
    each piece, so making an API server for dio to communicate with seems
    like a good next step.
    
    * The server side dio code should be moved into this
    * A REST client added to dio then hooked up to this API
    
    Once that's in place, update the API to use PG and Minio backend,
    which should give an end-to-end understanding of things.

--- api/api.go
+++ api/api.go
@@ -1,1 +1,43 @@
-package api
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+
+	rest "github.com/emicklei/go-restful"
+)
+
+func main() {
+	ws := new(rest.WebService)
+	ws.Filter(rest.NoBrowserCacheFilter)
+	ws.Route(ws.PUT("/db_upload").To(dbUpload))
+	ws.Route(ws.GET("/db_download").To(dbDownload))
+	ws.Route(ws.GET("/db_list").To(dbList))
+	rest.Add(ws)
+	http.ListenAndServe(":8080", nil)
+}
+
+// Upload a database
+// Can be tested with: curl -T a.db -H "Name: a.db" -w \%{response_code} -D headers.out http://localhost:8080/db_upload
+func dbUpload(r *rest.Request, w *rest.Response) {
+	dbName := r.Request.Header.Get("Name")
+
+	var buf bytes.Buffer
+	buf.ReadFrom(r.Request.Body)
+	log.Printf("Database uploaded.  Name: '%s', size: %d bytes\n", dbName, buf.Len())
+
+	// Send a 201 "Created" response, along with the location of the URL for working with the (new) database
+	w.AddHeader("Location", "/"+dbName)
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Download a database
+func dbDownload(r *rest.Request, w *rest.Response) {
+	log.Println("dbDownload() called")
+}
+
+// Get a list of databases
+func dbList(r *rest.Request, w *rest.Response) {
+	log.Println("dbList() called")
+}
