f6a24eaeb7d6f03c30f734e98728754bb49d69e0
Justin Clift <justin@postgresql.org>
2017-05-31 23:29:28 +0100

    Databases can now be downloaded using either a branch or commit
      $ dio pull e.db --commit 4de6210e4c681192506eb421edc9727d76246f3244ff71078b39bcaf8f45327f
      http://localhost:8080 - Database 'e.db' downloaded.  Size: 64512, commit: 4de6210e4c681192506eb421edc9727d76246f3244ff71078b39bcaf8f45327f
    
      $ dio pull e.db --branch master
      http://localhost:8080 - Database 'e.db' downloaded.  Size: 64512, branch: master

--- cmd/pull.go
+++ cmd/pull.go
@@ -11,6 +11,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var pullCmdBranch, pullCmdCommit string
+
 // Downloads a database from a DBHub.io cloud.
 var pullCmd = &cobra.Command{
 	Use:   "pull [database name]",
@@ -26,12 +28,23 @@ var pullCmd = &cobra.Command{
 			return errors.New("Only one database can be downloaded at a time (for now)")
 		}
 
+		// Ensure we were given either a branch name or commit ID (and not both)
+		if pullCmdBranch == "" && pullCmdCommit == "" {
+			return errors.New("Either a branch name or commit ID must be given.")
+		}
+		if pullCmdBranch != "" && pullCmdCommit != "" {
+			return errors.New("Either a branch name or commit ID must be given.  Not both!")
+		}
+
 		// Download the database file
 		file := args[0]
-		resp, body, errs := rq.New().Get(cloud+"/db_download").
-			Set("branch", branch).
-			Set("database", file).
-			End()
+		req := rq.New().Get(cloud+"/db_download").Set("database", file)
+		if pullCmdBranch != "" {
+			req.Set("branch", pullCmdBranch)
+		} else {
+			req.Set("commit", pullCmdCommit)
+		}
+		resp, body, errs := req.End()
 		if errs != nil {
 			log.Print("Errors when downloading database:")
 			for _, err := range errs {
@@ -41,6 +54,10 @@ var pullCmd = &cobra.Command{
 		}
 		if resp.StatusCode != http.StatusOK {
 			if resp.StatusCode == http.StatusNotFound {
+				if pullCmdCommit != "" {
+					return errors.New(fmt.Sprintf("Requested database not found with commit %s.",
+						pullCmdCommit))
+				}
 				return errors.New("Requested database not found")
 			}
 			return errors.New(fmt.Sprintf("Download failed with an error: HTTP status %d - '%v'\n",
@@ -52,14 +69,20 @@ var pullCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		fmt.Printf("%s - Database download successful.  Name: %s, size: %d, branch: %s\n", cloud,
-			file, len(body), branch)
+		if pullCmdBranch != "" {
+			fmt.Printf("%s - Database '%s' downloaded.  Size: %d, branch: %s\n", cloud, file, len(body),
+				pullCmdBranch)
+		} else {
+			fmt.Printf("%s - Database '%s' downloaded.  Size: %d, commit: %s\n", cloud, file, len(body),
+				pullCmdCommit)
+		}
 		return nil
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(pullCmd)
-	pullCmd.Flags().StringVar(&branch, "branch", "master",
+	pullCmd.Flags().StringVar(&pullCmdBranch, "branch", "",
 		"Remote branch the database will be downloaded from")
+	pullCmd.Flags().StringVar(&pullCmdCommit, "commit", "", "Commit ID of the database to download")
 }
