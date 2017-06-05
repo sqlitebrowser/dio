e6f9ac64a54143970335ecf58a60a843dba46ec0
Justin Clift <justin@postgresql.org>
2017-06-05 07:53:02 +0100

    Latest PG schema for dio backend
    This will likely change quite a bit over the next day or two, as
    different pieces are tried out

--- database/dio.sql
+++ database/dio.sql
@@ -1,40 +1,13 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.6.2
--- Dumped by pg_dump version 9.6.2
-
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- Name: database_versions; Type: TABLE; Schema: public; Owner: -
+-- Name: database_files; Type: TABLE; Schema: public; Owner: dbhub
 --
 
-CREATE TABLE database_versions (
-    "verID" integer NOT NULL,
-    "dbID" integer NOT NULL,
-    "branchHeads" jsonb NOT NULL,
-    tags jsonb,
+CREATE TABLE database_files (
+    sha256 text NOT NULL,
     "minioServer" text NOT NULL,
     "minioFolder" text NOT NULL,
     "minioID" text NOT NULL
@@ -42,48 +15,24 @@ CREATE TABLE database_versions (
 
 
 --
--- Name: database_versions_verID_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE "database_versions_verID_seq"
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: database_versions_verID_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE "database_versions_verID_seq" OWNED BY database_versions."verID";
-
-
---
--- Name: sqlite_databases; Type: TABLE; Schema: public; Owner: -
+-- Name: sqlite_databases; Type: TABLE; Schema: public; Owner: dbhub
 --
 
 CREATE TABLE sqlite_databases (
     "dbName" text NOT NULL,
     "dbDefaultBranch" text DEFAULT 'master'::text NOT NULL,
-    "commitList" jsonb NOT NULL
+    "commitList" jsonb NOT NULL,
+    "branchHeads" jsonb,
+    tags jsonb
 );
 
 
 --
--- Name: database_versions verID; Type: DEFAULT; Schema: public; Owner: -
+-- Name: database_files database_versions_pkey; Type: CONSTRAINT; Schema: public; Owner: dbhub
 --
 
-ALTER TABLE ONLY database_versions ALTER COLUMN "verID" SET DEFAULT nextval('"database_versions_verID_seq"'::regclass);
-
-
---
--- Name: database_versions database_versions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY database_versions
-    ADD CONSTRAINT database_versions_pkey PRIMARY KEY ("verID");
+ALTER TABLE ONLY database_files
+    ADD CONSTRAINT database_versions_pkey PRIMARY KEY (sha256);
 
 
 --
@@ -92,9 +41,3 @@ ALTER TABLE ONLY database_versions
 
 ALTER TABLE ONLY sqlite_databases
     ADD CONSTRAINT sqlite_databases_pkey PRIMARY KEY ("dbName");
-
-
---
--- PostgreSQL database dump complete
---
-
