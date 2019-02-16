be7162dbb654ca36e021e974ec294d8e9d0f740a
Justin Clift <justin@postgresql.org>
2019-02-16 17:11:17 +1100

    Move reading the client side TLS into the shared init code

--- cmd/list.go
+++ cmd/list.go
@@ -1,12 +1,9 @@
 package cmd
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 
 	rq "github.com/parnurzeal/gorequest"
 	"github.com/spf13/cobra"
@@ -17,41 +14,11 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Returns a list of available databases",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO: In the real code, we'd likely include things like # stars and fork count too
-
-		// Load our self signed CA chain
-		// TODO: Read the certificate from a proper location
-		ourCAPool := x509.NewCertPool()
-		chainFile, err := ioutil.ReadFile("/home/jc/git_repos/src/github.com/sqlitebrowser/dbhub.io/docker/certs/ca-chain-docker.cert.pem")
-		if err != nil {
-			fmt.Printf("Error opening Certificate Authority chain file: %v\n", err)
-			return err
-		}
-		ok := ourCAPool.AppendCertsFromPEM(chainFile)
-		if !ok {
-			fmt.Println("Error appending certificate file")
-			return errors.New("error appending certificate file")
-		}
-
-		// Load a client certificate file
-		// TODO: Read the certificate from a proper location
-		cert, err := tls.LoadX509KeyPair("/home/jc/default.cert.pem", "/home/jc/default.cert.pem")
-		if err != nil {
-			return err
-		}
-
-		// Load our self signed CA Cert chain, and set TLS1.2 as minimum
-		newTLSConfig := &tls.Config{
-			Certificates:             []tls.Certificate{cert},
-			ClientCAs:                ourCAPool,
-			MinVersion:               tls.VersionTLS12,
-			PreferServerCipherSuites: true,
-			RootCAs:                  ourCAPool,
-		}
+		// TODO: Include things like # stars and fork count too
 
 		// Retrieve the database list from the cloud
 		resp, body, errs := rq.New().
-			TLSClientConfig(newTLSConfig).
+			TLSClientConfig(&TLSConfig).
 			Get(cloud + "/default").
 			End()
 		if errs != nil {
@@ -63,7 +30,7 @@ var listCmd = &cobra.Command{
 		}
 		defer resp.Body.Close()
 		var list []dbListEntry
-		err = json.Unmarshal([]byte(body), &list)
+		err := json.Unmarshal([]byte(body), &list)
 		if err != nil {
 			fmt.Printf("Error retrieving database list: '%v'\n", err.Error())
 			return err
--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,22 @@
 package cmd
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var branch, cfgFile, cloud, commit, email, name, msg, tag string
+var (
+	branch, cfgFile, cloud, commit, email, name, msg, tag string
+	TLSConfig                                             tls.Config
+)
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
@@ -23,7 +30,7 @@ and manipulate its tags and branches.`,
 	SilenceUsage:  true,
 }
 
-// Execute adds all child commands to the root command sets flags appropriately.
+// Execute adds all child commands to the root command & sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
@@ -35,9 +42,37 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
-		"config file (default is $HOME/.dio.yaml)")
+		"config file (default is $HOME/.dio/config.yaml)")
 	RootCmd.PersistentFlags().StringVar(&cloud, "cloud", "https://docker-dev.dbhub.io:5550",
 		"Address of the DBHub.io cloud")
+
+	// Read our certificate info, if present
+	// TODO: Read the certificate from a proper location
+	ourCAPool := x509.NewCertPool()
+	chainFile, err := ioutil.ReadFile("/home/jc/git_repos/src/github.com/sqlitebrowser/dbhub.io/docker/certs/ca-chain-docker.cert.pem")
+	if err != nil {
+		fmt.Println(err)
+	}
+	ok := ourCAPool.AppendCertsFromPEM(chainFile)
+	if !ok {
+		fmt.Println("Error when loading certificate chain file")
+	}
+
+	// Load a client certificate file
+	// TODO: Read the certificate from a proper location
+	cert, err := tls.LoadX509KeyPair("/home/jc/default.cert.pem", "/home/jc/default.cert.pem")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Load our self signed CA Cert chain, and set TLS1.2 as minimum
+	TLSConfig = tls.Config{
+		Certificates:             []tls.Certificate{cert},
+		ClientCAs:                ourCAPool,
+		MinVersion:               tls.VersionTLS12,
+		PreferServerCipherSuites: true,
+		RootCAs:                  ourCAPool,
+	}
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -54,8 +89,8 @@ func initConfig() {
 		}
 
 		// Search config in home directory with name ".dio" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".dio")
+		viper.AddConfigPath(filepath.Join(home, ".dio"))
+		viper.SetConfigName("config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
