ffea9d5c6aac4ec4d90332856b101c6885004e94
Justin Clift <justin@postgresql.org>
2019-02-17 15:05:43 +1100

    Multiple licences for downloading can now be given on the command line

--- cmd/licenceGet.go
+++ cmd/licenceGet.go
@@ -20,44 +20,51 @@ var licenceGetCmd = &cobra.Command{
 		if len(args) == 0 {
 			return errors.New("No licence name specified")
 		}
-		// TODO: Allow giving multiple licence names on the command line.  Hopefully just needs turning this
-		// TODO  into a for loop.
-		// TODO: The key word "all" should be a short cut for downloading all of the licences
-		if len(args) > 1 {
-			return errors.New("Only one licence can be downloaded at a time (for now)")
-		}
+		// TODO: The key word "all" should be a short cut for downloading all of the licences.  When implemented, make
+		//       sure to add info about the all keyword to the help text
 
 		// Download the licence text
-		lic := args[0]
-		resp, body, errs := rq.New().TLSClientConfig(&TLSConfig).Get(cloud + "/licence/get").
-			Query(fmt.Sprintf("licence=%s", lic)).End()
-		if errs != nil {
-			log.Print("Errors when downloading licence text:")
-			for _, err := range errs {
-				log.Print(err.Error())
+		dlStatus := make(map[string]string)
+		for _, lic := range args {
+			resp, body, errs := rq.New().TLSClientConfig(&TLSConfig).Get(cloud + "/licence/get").
+				Query(fmt.Sprintf("licence=%s", lic)).End()
+			if errs != nil {
+				for _, err := range errs {
+					log.Print(err.Error())
+				}
+				dlStatus[lic] = "Error when downloading licence text"
+				continue
 			}
-			return errors.New("Error when downloading licence text")
-		}
-		if resp.StatusCode != http.StatusOK {
-			if resp.StatusCode == http.StatusNotFound {
-				return errors.New(fmt.Sprintf("Requested licence '%s' not found on %s", lic, cloud))
+			if resp.StatusCode != http.StatusOK {
+				if resp.StatusCode == http.StatusNotFound {
+					dlStatus[lic] = "Requested licence not found"
+					continue
+				}
+				dlStatus[lic] = fmt.Sprintf("Download failed with an error: HTTP status %d - '%v'",
+					resp.StatusCode, resp.Status)
+				continue
 			}
-			return errors.New(fmt.Sprintf("Download failed with an error: HTTP status %d - '%v'\n",
-				resp.StatusCode, resp.Status))
+
+			// Write the licence to disk
+			var ext string
+			if resp.Header.Get("Content-Type") == "text/html" {
+				ext = "html"
+			} else {
+				ext = "txt"
+			}
+			err := ioutil.WriteFile(fmt.Sprintf("%s.%s", lic, ext), []byte(body), 0644)
+			if err != nil {
+				dlStatus[lic] = err.Error()
+			}
+			dlStatus[lic] = fmt.Sprintf("Licence '%s.%s' downloaded", lic, ext)
 		}
 
-		// Write the licence to disk
-		var ext string
-		if resp.Header.Get("Content-Type") == "text/html" {
-			ext = "html"
-		} else {
-			ext = "txt"
+		// Display the status of the individual licence downloads
+		fmt.Printf("Licence downloading completed from: %s\n\n", cloud)
+		for i, j := range dlStatus {
+			fmt.Printf("  * %s: %s\n", i, j)
 		}
-		err := ioutil.WriteFile(fmt.Sprintf("%s.%s", lic, ext), []byte(body), 0644)
-		if err != nil {
-			return err
-		}
-		fmt.Printf("Licence '%s.%s' downloaded from %s.\n", lic, ext, cloud)
+		fmt.Println()
 		return nil
 	},
 }
