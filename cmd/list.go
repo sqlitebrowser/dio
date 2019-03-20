abd52a451f5b5f52b33a8f0d1fb48165ab561671
Justin Clift <justin@postgresql.org>
2019-03-21 00:35:21 +1100

    list: add redirection of output, for future test usage

--- cmd/list.go
+++ cmd/list.go
@@ -11,43 +11,68 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Returns the list of your databases on DBHub.io",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO: Include things like # stars and fork count too
-		// TODO: Add parameter for listing the (public) databases of other user(s) too
-
-		// Retrieve the database list for the user
-		dbList, err := getDatabases(cloud, certUser)
-		if err != nil {
-			return err
-		}
-
-		// Display the list of databases
-		if len(dbList) == 0 {
-			fmt.Printf("Cloud '%s' has no databases\n", cloud)
-			return nil
-		}
-		fmt.Printf("Databases on %s\n\n", cloud)
-		for _, j := range dbList {
-			fmt.Printf("  * Database: %s\n", j.Name)
-			if j.OneLineDesc != "" {
-				fmt.Printf("      Description: %s\n", j.OneLineDesc)
-			}
-			fmt.Printf("      Default branch: %s\n", j.DefBranch)
-			_, err := numFormat.Printf("      Size: %d bytes\n", j.Size)
-			if err != nil {
-				fmt.Println(err) // Not sure if this is the right approach
-			}
-			if j.Licence != "" {
-				fmt.Printf("      Licence: %s\n", j.Licence)
-			} else {
-				fmt.Println("      Licence: Not specified")
-			}
-			fmt.Printf("      File last modified: %s\n", j.LastModified)
-			fmt.Printf("      Repository last updated: %s\n\n", j.RepoModified)
-		}
-		return nil
+		return list(args)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(listCmd)
+}
+
+func list(args []string) error {
+	// TODO: Include things like # stars and fork count too
+	// TODO: Add parameter for listing the (public) databases of other user(s) too
+
+	// Retrieve the database list for the user
+	dbList, err := getDatabases(cloud, certUser)
+	if err != nil {
+		return err
+	}
+
+	// Display the list of databases
+	if len(dbList) == 0 {
+		_, err = fmt.Fprintf(fOut, "Cloud '%s' has no databases\n", cloud)
+		return err
+	}
+	fmt.Printf("Databases on %s\n\n", cloud)
+	for _, j := range dbList {
+		_, err = fmt.Fprintf(fOut, "  * Database: %s\n", j.Name)
+		if err != nil {
+			return err
+		}
+		if j.OneLineDesc != "" {
+			_, err = fmt.Fprintf(fOut, "      Description: %s\n", j.OneLineDesc)
+			if err != nil {
+				return err
+			}
+		}
+		_, err = fmt.Fprintf(fOut, "      Default branch: %s\n", j.DefBranch)
+		if err != nil {
+			return err
+		}
+		_, err := numFormat.Fprintf(fOut, "      Size: %d bytes\n", j.Size)
+		if err != nil {
+			return err
+		}
+		if j.Licence != "" {
+			_, err = fmt.Fprintf(fOut, "      Licence: %s\n", j.Licence)
+			if err != nil {
+				return err
+			}
+		} else {
+			_, err = fmt.Fprintf(fOut, "      Licence: Not specified")
+			if err != nil {
+				return err
+			}
+		}
+		_, err = fmt.Fprintf(fOut, "      File last modified: %s\n", j.LastModified)
+		if err != nil {
+			return err
+		}
+		_, err = fmt.Fprintf(fOut, "      Repository last updated: %s\n\n", j.RepoModified)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
